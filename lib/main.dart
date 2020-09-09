571cf9f0dda493d5f0c14204bf5c7d4ec7b8a846
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2020-09-09 12:35:53 -0300

    DeviceOrientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/route_manager.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:kepler/locale/translations.dart';
@@ -8,22 +9,22 @@ import 'package:syncfusion_flutter_core/core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  string.init();
+  await string.init();
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   SyncfusionLicense.registerLicense(
       "NT8mJyc2IWhia31hfWN9Z2doYmF8YGJ8ampqanNiYmlmamlmanMDHmg0JiAnMiU8PjImITowOjw3NjEyISE8IBM0PjI6P30wPD4=");
   runApp(GetMaterialApp(
     title: string.text('app_title'),
     defaultTransition: Transition.cupertino,
     theme: ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Color(0xFFFe0e0e0),
-      cardColor: Color(0xFFFe0e0e0),
-      textTheme: TextTheme(
-        headline4: TextStyle(
+        brightness: Brightness.light,
+        primaryColor: Color(0xFFFe0e0e0),
+        cardColor: Color(0xFFFe0e0e0),
+        textTheme: TextTheme(
+            headline4: TextStyle(
           color: Colors.black,
-        )
-      )
-    ),
+        ))),
     darkTheme: ThemeData(
       brightness: Brightness.dark,
       primaryColor: Color(0xFFF20211E),
