5672d5db038d66df903bcaabf28e10e23bd16640
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-09-19 16:17:51 +0700

    Added flare animation for stars

--- lib/widgets/planets/star.dart
+++ lib/widgets/planets/star.dart
@@ -1,4 +1,6 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:get/get.dart';
 import 'package:kepler/controllers/starsController.dart';
 
@@ -10,12 +12,27 @@ class Star extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: Get.width / 3.3,
-      height: Get.width / 3.3,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(360)),
-        shape: BoxShape.rectangle,
-        color: StarsController.to.getStarColor(temperature),
+      width: Get.width / 2.7,
+      height: Get.width / 2.7,
+      child: Stack(
+        children: [
+          FlareActor(
+            "assets/flare/shine.flr",
+            animation: "start",
+            fit: BoxFit.fitHeight,
+          ),
+          Center(
+            child: Container(
+              width: Get.width / 3.5,
+              height: Get.width / 3.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(360)),
+                shape: BoxShape.rectangle,
+                color: StarsController.to.getStarColor(temperature),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
