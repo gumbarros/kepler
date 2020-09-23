bc8aa80bc85f8e4c77544cacc8e72f3394d6afd5
Gustavo Barros <gustavomauriciodebarros@gmail.com>
2020-09-22 22:28:08 -0300

    Heroku

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const csv = require('csvtojson');
 const fs = require('fs');
  
 const url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,hostname,disc_year,pl_orbper,pl_radj,pl_dens,st_teff,st_rad,sy_kmag+from+pscomppars+where+pl_controv_flag=0&format=csv'
-
+const port = process.env.port || 3000
 //Entrypoint of the API
 app.get("/", function(req, res){
 
@@ -35,6 +35,6 @@ app.get("/", function(req, res){
     console.log("Error: " + err.message);
 });
 
-app.listen("3000", function(){
-    console.log("Server running on port 3000")
+app.listen(port, function(){
+    console.log("Server running on port " + port)
 })
