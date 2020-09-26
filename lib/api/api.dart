44f668e2492d542e76ea0c004fc3ca0e7dc346c3
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-09-26 22:09:51 +0700

    Update api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,7 +3,7 @@ import 'package:json_async/json_async.dart';
 
 
 class API {
-  static const String url = "https://kepler-api-42.herokuapp.com/";
+  static const String url = "https://kepler-api-1.herokuapp.com/";
 
   static Future<List> getAllData() async {
     final http.Response response = await http.get(url);
