807f290b4905135823f3e070476f6f57a66d8a9c
Erik Reynolds <erik.call.reynolds@gmail.com>
2020-09-29 13:52:16 -0400

    Added clustering to the orbit chart Made tracks strokes so they look like orbits

--- lib/views/charts/chartsView.dart
+++ lib/views/charts/chartsView.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
 import 'package:kepler/cupertinoPageRoute.dart';
-import 'package:kepler/locale/translations.dart';
 import 'package:kepler/views/charts/orbitChartView.dart';
 import 'package:kepler/widgets/cards/imageCard.dart';
 import 'package:kepler/widgets/header/header.dart';
--- lib/views/charts/orbitChartView.dart
+++ lib/views/charts/orbitChartView.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:k_means_cluster/k_means_cluster.dart';
 import 'package:kepler/controllers/chartsController.dart';
 import 'package:kepler/database/database.dart';
 import 'package:kepler/models/planetData.dart';
@@ -26,42 +29,138 @@ class OrbitChartView extends StatelessWidget {
                     context,
                   ),
                 ),
-                Container(
-                    height: Get.width,
-                    child: FutureBuilder<List<PlanetData>>(
-                      future: KeplerDatabase.db.getTopOrbits(
-                        limit: 50,
-                      ),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<PlanetData>> snapshot) {
-                        switch (snapshot.connectionState) {
-                          case ConnectionState.active:
-                          case ConnectionState.waiting:
-                            return Center(
-                              child: Loading(),
-                            );
-                          default:
-                            return SfCircularChart(
-                              series: <CircularSeries>[
-                                // Renders radial bar chart
-                                RadialBarSeries<PlanetData, String>(
-                                  dataSource: snapshot.data,
-                                  xValueMapper: (data, _) => data.planetName,
-                                  yValueMapper: (data, _) => data.orbitalPeriod,
-                                  dataLabelMapper: (data, _) => data.planetName,
-                                  dataLabelSettings: DataLabelSettings(
-                                      isVisible: true,
-                                      labelPosition:
-                                          ChartDataLabelPosition.inside),
-                                ),
-                              ],
-                            );
-                        }
-                      },
-                    ))
+                OrbitBuilder(
+                  future: KeplerDatabase.db.getTopOrbits(
+                    limit: 500,
+                  ),
+                )
               ],
             ),
           );
         });
+  }
+}
+
+class OrbitBuilder extends StatelessWidget {
+  final Future<List<PlanetData>> future;
+
+  OrbitBuilder({
+    Key key,
+    @required this.future,
+  }) : super(key: key);
+
+  getClusters(List<PlanetData> planets, int clusterCount) async {
+    // Set the distance measure; this can be any function of the form
+    // num f(List<num> a, List<num> b): a and b contain the coordinates
+    // of two instances; f returns a numerical distance between the
+    // points.
+    distanceMeasure = DistanceType.squaredEuclidian;
+
+    // Create the list of instances.
+    List<Instance> instances = planets
+        .map((PlanetData planet) =>
+            Instance([planet.orbitalPeriod, 0], id: planet.planetName))
+        .toList();
+
+    // Randomly create the initial clusters.
+    List<Cluster> clusters = initialClusters(clusterCount, instances, seed: 0);
+
+    // Run the algorithm.
+    var info = kmeans(clusters: clusters, instances: instances);
+    clusters.sort(
+      (a, b) => getMaxForCluster(
+        a,
+      ).compareTo(
+        getMaxForCluster(
+          b,
+        ),
+      ),
+    );
+    return clusters;
+  }
+
+  num getMaxForClusterList(List<Cluster> clusterlist) => clusterlist
+      .map(
+        (cluster) => getMaxForCluster(cluster),
+      )
+      .reduce(
+        max,
+      );
+  num getMinForClusterList(List<Cluster> clusterlist) => clusterlist
+      .map(
+        (cluster) => getMinForCluster(cluster),
+      )
+      .reduce(
+        min,
+      );
+  num getMaxForCluster(Cluster cluster) => cluster.instances
+      .map(
+        (planet) => planet.location.first,
+      )
+      .reduce(
+        max,
+      );
+  num getMinForCluster(Cluster cluster) => cluster.instances
+      .map(
+        (planet) => planet.location.first,
+      )
+      .reduce(
+        min,
+      );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: Get.width,
+        child: FutureBuilder<List<Cluster>>(
+          future: this.future.then(
+                (List<PlanetData> planetData) async => (planetData.length > 10)
+                    ? await this.getClusters(planetData, 10)
+                    : await this.getClusters(planetData, 1),
+              ),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Cluster>> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Loading(),
+                );
+              default:
+                List<Cluster> clusters = snapshot.data;
+                final maxRadius = getMaxForClusterList(clusters);
+                return SfCircularChart(
+                  series: <CircularSeries>[
+                    // Renders radial bar chart
+                    RadialBarSeries<Cluster, String>(
+                      gap: '9%',
+                      innerRadius: '0%',
+                      radius: '100%',
+                      dataSource: snapshot.data,
+                      pointRadiusMapper: (data, _) =>
+                          "${getMaxForCluster(data) / maxRadius * 100}%",
+                      // maximumValue: snapshot.data
+                      //     .map(
+                      //       (cluster) => cluster.instances
+                      //           .map(
+                      //             (planet) => planet.location.first,
+                      //           )
+                      //           .reduce(max),
+                      //     )
+                      //     .reduce(max),
+
+                      maximumValue: 100,
+                      xValueMapper: (data, _) => data.id,
+                      // yValueMapper: (data, _) => data.location.first,
+                      yValueMapper: (data, _) => 100,
+                      dataLabelMapper: (data, _) => data.id,
+                      dataLabelSettings: DataLabelSettings(
+                          isVisible: true,
+                          labelPosition: ChartDataLabelPosition.inside),
+                    ),
+                  ],
+                );
+            }
+          },
+        ));
   }
 }
