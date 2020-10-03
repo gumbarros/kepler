447fbd878818ec462921f5fe8a74ab7d7e41dcec
Gustavo Barros <gustavomauriciodebarros@gmail.com>
2020-10-03 10:50:55 -0300

    First time sync and secret developer menu

--- lib/controllers/homeController.dart
+++ lib/controllers/homeController.dart
@@ -1,7 +1,18 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:kepler/controllers/settingsController.dart';
+import 'package:kepler/database/database.dart';
 
 class HomeController extends GetxController {
   static HomeController get to => Get.find();
+  final GetStorage getStorage = GetStorage();
+  @override
+  void onReady() {
+    if(getStorage.read("welcome") == null){
+      SettingsController.to.updateData();
+      getStorage.write("welcome", true);
+    }
+  }
 
   void upd() {
     update();
