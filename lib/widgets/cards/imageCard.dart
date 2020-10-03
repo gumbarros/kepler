94422d00b70bd22fdf2e73797ed98a1114495fac
Gustavo Barros <gustavomauriciodebarros@gmail.com>
2020-10-03 08:47:20 -0300

    Restored menu cards

--- lib/widgets/cards/imageCard.dart
+++ lib/widgets/cards/imageCard.dart
@@ -1,85 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:get/route_manager.dart';
 
-class ImageCard extends StatefulWidget {
+class ColorsCard extends StatelessWidget {
   final Function onTap;
   final String text;
   final List<Color> colorList;
   final Widget child;
-  final double width;
-  final double height;
-  final String image;
 
-  ImageCard(
-      {@required this.onTap,
-      @required this.text,
-        this.image,
-      this.colorList,
-      this.child,
-      this.height,
-      this.width});
-
-  @override
-  _ImageCardState createState() => _ImageCardState();
-}
-
-class _ImageCardState extends State<ImageCard> with TickerProviderStateMixin {
-  Animation _scaleanimation;
-  AnimationController _scalecontroller;
-
-  @override
-  void initState() {
-    _scalecontroller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 100));
-    _scaleanimation = Tween<double>(
-      begin: 1,
-      end: 0.97,
-    ).animate(
-      _scalecontroller,
-    );
-    super.initState();
-  }
+  ColorsCard(
+      {@required this.onTap, @required this.text, this.colorList, this.child});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
-        await _scalecontroller.forward();
-        await _scalecontroller.reverse();
-        widget.onTap();
+        onTap();
       },
-      child: ScaleTransition(
-        scale: _scaleanimation,
-        child: Container(
-          height: Get.height / 7,
-          width: Get.width - 30,
-          decoration: BoxDecoration(
-              borderRadius:
-              BorderRadius.all(Radius.circular(10)),
-              image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.4),
-                      BlendMode.darken),
-                  fit: BoxFit.fitWidth,
-                  image:
-                  AssetImage('${widget.image}'))),
-          child: Center(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 10,
+      child: Stack(
+        children: [
+          Container(
+            height: Get.height / 8,
+            width: Get.width - 20,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(colors: colorList)),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                        fontFamily: "JosefinSans",
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                Text(
-                  '${widget.text}',
-                  style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'JosefinSans'),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          SizedBox(),
+        ],
       ),
     );
   }
