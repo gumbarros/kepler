646875516f282f499ea1035911ca0f27db1af148
Gustavo Barros <gustavomauriciodebarros@gmail.com>
2020-10-03 12:02:56 -0300

    Special timezone tratative

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -16,11 +16,24 @@ class API {
   }
 
   static Future<DailyImageData> getImageOfTheDay() async{
-    final http.Response response = await http.get(dailyUrl);
-    print("HTTP GET - " + dailyUrl);
-    final Map data = await jsonDecodeAsyncMap(response.body);
-    final DailyImageData image = DailyImageData.fromMap(data);
-    return image;
+    try{
+      final http.Response response = await http.get(dailyUrl);
+      print("HTTP GET - " + dailyUrl);
+      final Map data = await jsonDecodeAsyncMap(response.body);
+      final DailyImageData image = DailyImageData.fromMap(data);
+      dynamic a;
+      if(a.isNull) print("pam");
+      return image;
+    }
+    catch(e){
+      //There is a bug in NASA API in Brazil and other countries in this timezone in 23:00~00:00 hours
+      final String specialUrl = dailyUrl+ "&date="+DateTime.now().subtract(Duration(days: 1)).toString().substring(0,10);
+      final http.Response response = await http.get(specialUrl);
+      print("HTTP GET - " + specialUrl);
+      final Map data = await jsonDecodeAsyncMap(response.body);
+      final DailyImageData image = DailyImageData.fromMap(data);
+      return image;
+    }
   }
 
 }
